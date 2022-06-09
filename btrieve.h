475dd9f55a479872e08e79777402803f8068ba3b
Ryan Zander <exide@hotmail.com>
2022-06-09 01:07:40 -0700

    Skip a couple bytes per record on a certain format/record type combo.

--- btrieve.h
+++ btrieve.h
@@ -5,11 +5,15 @@
 
 enum Format { Old, New };
 
+enum RecordType { FixedLength, CompressedFixedLength, VariableLength, CompressedVariableLength, TruncatedVariableLength, VariableTailAllocationTable };
+
 struct BtrieveFile {
     enum Format format;
+    size_t file_size;
     uint32_t page_count;
     uint16_t page_size;
     uint32_t record_count;
+    enum RecordType record_type;
     int16_t physical_length;
     void* memory;
 };
@@ -31,10 +35,10 @@ int load(FILE* file, struct BtrieveFile* btrieve_file) {
         if (exit_code != 0) return 1;
     }
 
-    size_t file_size = ftell(file);
-    if (file_size == -1) return 1;
+    btrieve_file->file_size = ftell(file);
+    if (btrieve_file->file_size == -1) return 1;
 
-    btrieve_file->memory = malloc(file_size);
+    btrieve_file->memory = malloc(btrieve_file->file_size);
 
     {
         uint16_t exit_code = fseek(file, 0x00, SEEK_SET);
@@ -42,7 +46,7 @@ int load(FILE* file, struct BtrieveFile* btrieve_file) {
     }
     {
         uint16_t items_requested = 1;
-        uint16_t items_read = fread(btrieve_file->memory, file_size, items_requested, file);
+        uint16_t items_read = fread(btrieve_file->memory, btrieve_file->file_size, items_requested, file);
         if (items_read < items_requested) return 1;
     }
 
@@ -82,6 +86,30 @@ int load(FILE* file, struct BtrieveFile* btrieve_file) {
     // read 4 bytes @ 0x26 - number of pages in file (word-swapped)
     btrieve_file->page_count = swap_words(*(int32_t*)((size_t)fcr + 0x26));
 
+    int8_t virtual_records_ok = *(int8_t*)((size_t)fcr + 0x38);
+    int16_t user_flags = *(int16_t*)((size_t)fcr + 0x106);
+
+    if (user_flags & 8 && (virtual_records_ok || user_flags & 1)) {
+        btrieve_file->record_type = CompressedVariableLength;
+    }
+    else if (btrieve_file->format == New && user_flags & 0x0800) {
+        btrieve_file->record_type = VariableTailAllocationTable;
+    }
+    else if (user_flags & 8) {
+        btrieve_file->record_type = CompressedFixedLength;
+    }
+    else if (virtual_records_ok || user_flags & 1) {
+        if ((uint8_t)virtual_records_ok == 0x00FD || user_flags & 2) {
+            btrieve_file->record_type = TruncatedVariableLength;
+        }
+        else {
+            btrieve_file->record_type = VariableLength;
+        }
+    }
+    else {
+        btrieve_file->record_type = FixedLength;
+    }
+
     return 0;
 }
 
@@ -91,16 +119,18 @@ int load(FILE* file, struct BtrieveFile* btrieve_file) {
 int get_records(struct BtrieveFile* btrieve_file, void* buffer) {
     size_t current_record_index = 0;
     size_t page_index_after_fcr = btrieve_file->format == Old ? 1 : 2;
+    size_t total_pages = btrieve_file->file_size / btrieve_file->page_size;
 
-    for (size_t page_index = page_index_after_fcr; page_index < btrieve_file->page_count; ++page_index) {
+    for (size_t page_index = page_index_after_fcr; page_index < total_pages; ++page_index) {
 
         size_t page_offset = btrieve_file->page_size * page_index;
-        uint16_t page_usage = *(uint16_t*)((size_t)btrieve_file->memory + page_offset + 0x04);
+        uint32_t page_header = *(uint32_t*)((size_t)btrieve_file->memory + page_offset);
+        uint16_t page_usage = *(uint16_t*)((size_t)btrieve_file->memory + page_offset + sizeof(page_header));
 
-        int is_data_page = page_usage & 0x8000;
+        uint16_t is_data_page = page_usage & 0x8000;
         if (!is_data_page) continue;
 
-        size_t page_header_size = 0x06;
+        size_t page_header_size = sizeof(page_header) + sizeof(page_usage);
         size_t usable_page_size = btrieve_file->page_size - page_header_size;
         size_t records_on_this_page = usable_page_size / btrieve_file->physical_length;
 
@@ -112,13 +142,26 @@ int get_records(struct BtrieveFile* btrieve_file, void* buffer) {
                 break;
             }
 
-            size_t source_offset = page_offset + page_header_size + (record_index_on_page * btrieve_file->physical_length);
-            void* source_address = (void*)((size_t)btrieve_file->memory + source_offset);
+            size_t record_offset = page_offset + page_header_size + (record_index_on_page * btrieve_file->physical_length);
 
             size_t destination_offset = current_record_index * btrieve_file->physical_length;
             void* destination_address = (void*)((size_t)buffer + destination_offset);
 
-            memcpy(destination_address, source_address, btrieve_file->physical_length);
+            if (btrieve_file->record_type != CompressedFixedLength && btrieve_file->format == New) {
+                int16_t test = *(int16_t*)((size_t)btrieve_file->memory + record_offset);
+                if (test) {
+                    void* source_address = (void*)((size_t)btrieve_file->memory + record_offset + sizeof(test));
+                    memcpy(destination_address, source_address, btrieve_file->physical_length - sizeof(test));
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                void* source_address = (void*)((size_t)btrieve_file->memory + record_offset);
+                memcpy(destination_address, source_address, btrieve_file->physical_length);
+            }
+
             current_record_index++;
         }
     }
